5ec254dedb9ce2b44d515bfe1b71345f4668ade3
jnwnddh <98944538+jnwnddh@users.noreply.github.com>
2022-10-18 20:00:15 +0900

    에드폼추가

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -3,17 +3,15 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Todo from "../pages/Todo";
 
-
-const Router =() => {
-    return(
-        <BrowserRouter>
-            <Routes>
-                {/* <Route path='/' element={}/> */}
-                <Route path='/works/:id' element={<Todo />} />
-
-            </Routes>
-        </BrowserRouter>
-    );
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* <Route path='/' element={}/> */}
+        <Route path="/works/:id" element={<Todo />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default Router;
